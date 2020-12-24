0f6a735438cdd749b464ad9cbe0f16ae01bc298c
Peter Ahrens <ptrahrens@gmail.com>
2020-12-24 17:20:43 -0500

    fixes

--- src/AlternatingPacker.jl
+++ src/AlternatingPacker.jl
@@ -35,8 +35,7 @@ struct SymmetricPacker{Mtds}
     mtds::Mtds
 end
 
-SymmetricPacker(mtd) = SymmetricPacker{Tuple{typeof(mtd)}}((mtd,))
-SymmetricPacker(mtds...) = SymmetricPacker{typeof(mtds)}(mtds)
+SymmetricPacker(mtds...) = new{typeof(mtds)}(mtds)
 
 function pack_plaid(A::SparseMatrixCSC, method::SymmetricPacker; adj_A = nothing, kwargs...)
     if adj_A === nothing
