ab5c7623787c3e4ae0e11db2298c76774e41529c
Peter Ahrens <ptrahrens@gmail.com>
2021-04-09 16:45:21 -0400

    turn all the tests back on

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,12 +11,12 @@ end
 
 @testset "ChainPartitioners" begin
     Random.seed!(0xDEADBEEF)
-    #include("test_util.jl")
+    include("test_util.jl")
     include("test_SparsePrefixMatrices.jl")
-    #include("test_EnvelopeMatrices.jl")
-    #include("test_Partitions.jl")
+    include("test_EnvelopeMatrices.jl")
+    include("test_Partitions.jl")
     include("test_SparseColorArrays.jl")
-    #include("test_Costs.jl")
-    #include("test_Partitioners.jl")
-    #include("test_CuthillMcKee.jl")
+    include("test_Costs.jl")
+    include("test_Partitioners.jl")
+    include("test_CuthillMcKee.jl")
 end
