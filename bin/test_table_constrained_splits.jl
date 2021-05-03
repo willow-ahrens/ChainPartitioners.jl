ad8528ee1e0a2c2515ad4f7b8c001a354f842a34
Peter Ahrens <ptrahrens@gmail.com>
2021-05-03 16:08:03 -0400

    hmm

--- bin/test_table_constrained_splits.jl
+++ bin/test_table_constrained_splits.jl
@@ -9,11 +9,11 @@ using Cthulhu
 using Profile
 
 for mtx in [
-            "DIMACS10/chesapeake",
+            #"DIMACS10/chesapeake",
             #"HB/can_292",
             "Boeing/ct20stif",
-            "Schmid/thermal1",
-            "Rothberg/3dtube",
+            #"Schmid/thermal1",
+            #"Rothberg/3dtube",
            ]
     A = permutedims(1.0 * sparse(mdopen(mtx).A))
     (m, n) = size(A)
@@ -22,28 +22,37 @@ for mtx in [
     x = rand(n)
     ref_time = @belapsed(mul!($y, $A, $x))
 
-    mdl = ConstrainedCost(AffineConnectivityModel{Int64}(0, 0, 0, 1), VertexCount(), cld(n, 8))
-
-    #println()
-    #println()
-    #println(spy(A, maxwidth=50, maxheight=50, title="$mtx"))
     println(mtx)
     rows = []
-    for (key, method) in [
-        ("dynamic16", DynamicTotalSplitter(mdl)),
-        ("dynamic′16", DynamicTotalChunker(mdl)),
-        ("quadrangle16", ConvexTotalSplitter(mdl)),
+    for K = [4, 8, 16]
+        mdl = ConstrainedCost(AffineConnectivityModel{Int64}(0, 0, 0, 1), VertexCount(), ceil(Int, n/K * 1.5))
 
-    ]
-        setup_time = @belapsed(partition_stripe($A, 16, $method))/ref_time
-        Φ = partition_stripe(A, 16, method)
-        #@descend pack_stripe(A, method)
-        comm = total_value(A, Φ, mdl)
-        @assert issorted(Φ.spl)
-        @assert Φ.spl[1] == 1
-        @assert Φ.spl[end] == n + 1
+        (j_lo, j′_hi) = ChainPartitioners.column_constraints(A, K, mdl.w, mdl.w_max)
+        show(histogram(j′_hi .- j_lo, nbins = 15, closed = :left))
+        println()
 
-        push!(rows, [key setup_time comm])
+        #println()
+        #println()
+        #println(spy(A, maxwidth=50, maxheight=50, title="$mtx"))
+        for (key, method) in [
+            ("dynamic", DynamicTotalSplitter(mdl)),
+            ("dynamic′", DynamicTotalChunker(mdl)),
+            ("quadrangle", ConvexTotalSplitter(mdl)),
+
+        ]
+            setup_time = @belapsed(partition_stripe($A, $K, $method))/ref_time
+            Φ = nothing
+            #@profile begin
+                Φ = partition_stripe(A, K, method)
+            #end
+            #Profile.print()
+            comm = total_value(A, Φ, mdl)
+            @assert issorted(Φ.spl)
+            @assert Φ.spl[1] == 1
+            @assert Φ.spl[end] == n + 1
+
+            push!(rows, [key K setup_time comm])
+        end
     end
-    pretty_table(vcat(rows...), ["method", "setuptime", "communication"])
+    pretty_table(vcat(rows...), ["method", "K", "setuptime", "communication"])
 end
