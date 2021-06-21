9b78face5798331d1559f8789f8f1e725b85a99a
Documenter.jl <documenter@juliadocs.github.io>
2021-06-21 00:46:19 +0000

    build based on 0b13046

---
+++ v1.1.6/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ChainPartitioners","category":"page"},{"location":"#ChainPartitioners","page":"Home","title":"ChainPartitioners","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Some test documentation","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ChainPartitioners]","category":"page"}]
+}
--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
   "v2007.16192",
+  "v1.1",
   "v1.0",
   "dev",
 ];
